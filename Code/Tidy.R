44cb0356af5775754a34b8359e2e51f4fb5139d3
JanuaryRaven <kdickens@smu.edu>
2017-11-27 13:06:31 -0600

    Updated Tidy.R file

--- Code/Tidy.R
+++ Code/Tidy.R
@@ -1,1 +1,82 @@
 rawdata<-read.csv("Procrastination.csv", header=TRUE)
+
+nrow(rawdata)
+ncol(rawdata)
+
+tidydata<-rawdata
+
+colnames(tidydata)<-c("Age", "Gender", "Kids", "Education", "WorkStatus", "AnnIncome", "Occupation","TenureY",
+                     "TenureM", "CommSize", "Country", "Marital", "Sons", "Daughters", "DP1", "DP2", "DP3", "DP4", "DP5",
+                     "AIP1", "AIP2", "AIP3", "AIP4", "AIP5", "AIP6", "AIP7", "AIP8", "AIP9", "AIP10", "AIP11", "AIP12",
+                     "AIP13", "AIP14", "AIP15", "GP1", "GP2", "GP3", "GP4", "GP5", "GP6", "GP7", "GP8", "GP9", "GP10",
+                     "GP11", "GP12", "GP13", "GP14", "GP15", "GP16", "GP17", "GP18", "GP19", "GP20", "SWLS1", "SWLS2",
+                     "SWLS3", "SWLS4", "SWLS5", "SelfP", "OthersP")
+
+char<-c("Gender", "Kids", "Education", "WorkStatus", "Occupation", "CommSize", "Country", "Marital", "Sons", "SelfP", "OthersP")
+tidydata[char] <- sapply(tidydata[char], as.factor)
+
+tidydata$TenureY<-round(tidydata$TenureY)
+tidydata$TenureY<-as.integer(tidydata$TenureY)
+tidydata$TenureY[tidydata$TenureY ==999] <- NA
+
+tidydata$Country[tidydata$Country == '0'] <- ""
+
+tidydata$Sons[tidydata$Sons == 'Male'] <- 1
+tidydata$Sons[tidydata$Sons == 'Female'] <- 2
+tidydata$Sons<-as.integer(tidydata$Sons)
+
+tidydata$Occupation<-trimws(tidydata$Occupation, which=c("left"))
+tidydata$Occupation <- gsub("(?<=^| )([a-z])", "\\U\\1", tolower(tidydata$Occupation), perl = T)
+tidydata$Occupation[tidydata$Occupation == '0'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'Please Specify'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'Na'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'Ouh'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'Fdsdf'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'Abc'] <- ""
+tidydata$Occupation[tidydata$Occupation == 'S'] <- ""
+tidydata$Occupation[grepl("Teacher", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("Educator", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("Professor", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("College Faculty", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("Profucer", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("Education Specialist", tidydata$Occupation, ignore.case=FALSE)] <- "Educator"
+tidydata$Occupation[grepl("Assistant", tidydata$Occupation, ignore.case=FALSE)] <- "Assistant"
+tidydata$Occupation[grepl("Asst", tidydata$Occupation, ignore.case=FALSE)] <- "Assistant"
+tidydata$Occupation[grepl("Student", tidydata$Occupation, ignore.case=FALSE)] <- "Student"
+tidydata$Occupation[grepl("Doctoral", tidydata$Occupation, ignore.case=FALSE)] <- "Student"
+tidydata$Occupation[grepl("Postdoc", tidydata$Occupation, ignore.case=FALSE)] <- "Student"
+tidydata$Occupation[grepl("Studey", tidydata$Occupation, ignore.case=FALSE)] <- "Student"
+tidydata$Occupation[grepl("School", tidydata$Occupation, ignore.case=FALSE)] <- "Student"
+tidydata$Occupation[grepl("Nurse", tidydata$Occupation, ignore.case=FALSE)] <- "Nurse"
+tidydata$Occupation[grepl("RN", tidydata$Occupation, ignore.case=FALSE)] <- "Nurse"
+tidydata$Occupation[grepl("LPN", tidydata$Occupation, ignore.case=FALSE)] <- "Nurse"
+tidydata$Occupation[grepl("CRNA", tidydata$Occupation, ignore.case=FALSE)] <- "Nurse"
+tidydata$Occupation[grepl("Programmer", tidydata$Occupation, ignore.case=FALSE)] <- "Programmer"
+tidydata$Occupation[grepl("Developer", tidydata$Occupation, ignore.case=FALSE)] <- "Programmer"
+tidydata$Occupation[grepl("Engineer", tidydata$Occupation, ignore.case=FALSE)] <- "Engineer"
+tidydata$Occupation[grepl("Consultant", tidydata$Occupation, ignore.case=FALSE)] <- "Consultant"
+tidydata$Occupation[grepl("Businesswoman", tidydata$Occupation, ignore.case=FALSE)] <- "Business"
+tidydata$Occupation[grepl("Sales", tidydata$Occupation, ignore.case=FALSE)] <- "Sales"
+tidydata$Occupation[grepl("Mktg", tidydata$Occupation, ignore.case=FALSE)] <- "Marketing"
+tidydata$Occupation[grepl("Supervisor", tidydata$Occupation, ignore.case=FALSE)] <- "Supervisor"
+tidydata$Occupation[grepl("Coordinatore Operativo", tidydata$Occupation, ignore.case=FALSE)] <- "Operational Coordinator"
+tidydata$Occupation[grepl("MD", tidydata$Occupation, ignore.case=FALSE)] <- "Doctor"
+tidydata$Occupation[grepl("VMD", tidydata$Occupation, ignore.case=FALSE)] <- "Veternarian"
+tidydata$Occupation[grepl("Psychologis", tidydata$Occupation, ignore.case=FALSE)] <- "Psychologist"
+tidydata$Occupation[grepl("Gove Service", tidydata$Occupation, ignore.case=FALSE)] <- "Civil Service"
+tidydata$Occupation[grepl("Government", tidydata$Occupation, ignore.case=FALSE)] <- "Civil Service"
+tidydata$Occupation[grepl("Vidoe", tidydata$Occupation, ignore.case=FALSE)] <- "Video"
+
+tidydata$DPMean <- rowMeans(subset(tidydata, select = c(DP1, DP2, DP3, DP4, DP5)), na.rm = TRUE)
+tidydata$AIPMean <- rowMeans(subset(tidydata, select = c(AIP1, AIP2, AIP3, AIP4, AIP5, AIP6, AIP7, AIP8, AIP9, AIP10, AIP11, AIP12, AIP13, AIP14, AIP15)), na.rm = TRUE)
+tidydata$GPMean <- rowMeans(subset(tidydata, select = c(GP1, GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15, GP16, GP17, GP18, GP19, GP20)), na.rm = TRUE)
+tidydata$SWLSMean <- rowMeans(subset(tidydata, select = c(SWLS1, SWLS2, SWLS3, SWLS4, SWLS5)), na.rm = TRUE)
+tidydata$AIPMean<-round(tidydata$AIPMean, digits=0)
+
+NATidy<-sapply(tidydata, function(y) sum(length(which(is.na(y)))))
+summary(tidydata)
+str(tidydata)
+NATidy
+
+
+
